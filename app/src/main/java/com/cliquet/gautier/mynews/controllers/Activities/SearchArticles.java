8fb8b7ffa1f310c8b369bda8dc957c15ee8a29e5
Ocelot <gcliquet@gmail.com>
2019-06-05 18:39:39 +0200

    the code of checkboxes for searching articles is generalize to simplify the addition of new checkboxes

--- app/src/main/java/com/cliquet/gautier/mynews/controllers/Activities/SearchArticles.java
+++ app/src/main/java/com/cliquet/gautier/mynews/controllers/Activities/SearchArticles.java
@@ -7,15 +7,29 @@ import android.widget.CheckBox;
 
 import com.cliquet.gautier.mynews.R;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SearchArticles extends AppCompatActivity {
+public class SearchArticles extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.activity_search_articles_politics_checkbox)
-    CheckBox usCheckbox;
+    @BindView(R.id.activity_search_articles_arts_checkbox)
+    CheckBox artsCheckbox;
     @BindView(R.id.activity_search_articles_business_checkbox)
     CheckBox businessCheckbox;
+    @BindView(R.id.activity_search_articles_entrepreneurs_checkbox)
+    CheckBox entrepreneursCheckbox;
+    @BindView(R.id.activity_search_articles_politics_checkbox)
+    CheckBox politicsCheckbox;
+    @BindView(R.id.activity_search_articles_sports_checkbox)
+    CheckBox sportsCheckbox;
+    @BindView(R.id.activity_search_articles_travel_checkbox)
+    CheckBox travelCheckbox;
+
+
+    ArrayList<String> strSearchCheck = new ArrayList<>();
+    String strCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +37,46 @@ public class SearchArticles extends AppCompatActivity {
         setContentView(R.layout.activity_search_articles);
         ButterKnife.bind(this);
 
-        usCheckbox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (usCheckbox.isChecked()) {
-                    businessCheckbox.setChecked(true);
-                    usCheckbox.setText("OK!!!!");
-                }
-                else {
-                    usCheckbox.setText("U.S.");
-                }
-            }
-        });
+        artsCheckbox.setOnClickListener(this);
+        businessCheckbox.setOnClickListener(this);
+        entrepreneursCheckbox.setOnClickListener(this);
+        politicsCheckbox.setOnClickListener(this);
+        sportsCheckbox.setOnClickListener(this);
+        travelCheckbox.setOnClickListener(this);
+    }
+
+    //verify if a checkbox is check or uncheck
+    private void verifyCheckboxState(boolean booleanCheckbox, String strCheck) {
+        if (booleanCheckbox) {
+            addCheckedCheckbox(strCheck);
+        } else {
+            removeUncheckedCheckbox(strCheck);
+        }
+    }
+
+    //remove the term of the unchecked checkbox of the arraylist
+    private void removeUncheckedCheckbox(String strCheck) {
+        for(int i = 0; i <= strSearchCheck.size(); i++) {
+                        if (strSearchCheck.get(i).equals(strCheck)) {
+                            strSearchCheck.remove(i);
+                            break;
+                        }
+                    }
+    }
+
+    //add the term associated with the checked checkbox in the arraylist
+    private void addCheckedCheckbox (String strCheck) {
+        strSearchCheck.add(strCheck);
+    }
+
+    //get the id of the clicked checkbox
+    @Override
+    public void onClick(View v) {
+        int idCheckbox = v.getId();
+        CheckBox mCheckBox = findViewById(idCheckbox);
+
+        boolean booleanCheckbox = mCheckBox.isChecked();
+        strCheck = mCheckBox.getText().toString();
+        verifyCheckboxState(booleanCheckbox, strCheck);
     }
 }
