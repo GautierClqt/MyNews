4dabe726ee2a27c1315a3599bb9d1428df3b4903
Ocelot <gcliquet@gmail.com>
2019-09-02 20:54:03 +0200

    commit before trying Travis

---
+++ app/src/androidTest/java/com/cliquet/gautier/mynews/controllers/Activities/FirstTest.java
@@ -0,0 +1,70 @@
+package com.cliquet.gautier.mynews.controllers.Activities;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.cliquet.gautier.mynews.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class FirstTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickOnArticle() {
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.layout_item_parentLayout_relativeLayout),
+                        childAtPosition(
+                                allOf(withId(R.id.fragment_top_stories_recyclerview),
+                                        childAtPosition(
+                                                withId(R.id.fragment_top_stories_rootview),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        relativeLayout.perform(click());
+
+        pressBack();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
